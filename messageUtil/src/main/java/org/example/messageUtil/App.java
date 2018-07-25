b36b3d8f473c95a8d43536690d5460782d134b71
Deval Sheth <deval.sheth@einfochips.com>
2018-07-25 18:00:53 +0530

    new proj

---
+++ messageUtil/src/main/java/org/example/messageUtil/App.java
@@ -0,0 +1,13 @@
+package org.example.messageUtil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
